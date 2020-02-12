6d73fbaf41e1d2378c450714316d29e07111d1c0
lijian <lijianwzx@163.com>
2020-02-12 15:07:53 +0800

    1.0

--- application/admin/controller/Base.php
+++ application/admin/controller/Base.php
@@ -3,6 +3,7 @@
 namespace app\admin\controller;
 
 use think\Controller;
+
 class Base extends Controller
 {
     public function initialize()
@@ -15,15 +16,19 @@ class Base extends Controller
         $module = request()->module(); //获取当前访问的模块
         $controller = request()->controller(); //获取当前访问的控制器
         $action = request()->action(); //获取当前访问的方法
-        $url = $module . '/' . $controller . '/' . $action; //转成字符串
+        $redirect_url = $module . '/' . $controller . '/' . $action; //转成字符串
+
         /**
          *验证是否登录
          */
-        //当前访问的页面$current_auth_str转为全小写后,如果不在$exception_arth_list客户中就验证用户是否登陆
-        if (!empty($exception_arth_list) && is_array($exception_arth_list)) {
-            if (!in_array(strtolower($redirect_url), $exception_arth_list)) {
-                $this->redirect("admin/Index/login");
+        if (!session("?admin.id")) {
+            //当前访问的页面$current_auth_str转为全小写后,如果不在$exception_arth_list客户中就验证用户是否登陆
+            if (!empty($exception_arth_list) && is_array($exception_arth_list)) {
+                if (!in_array(strtolower($redirect_url), $exception_arth_list)) {
+                    $this->redirect("admin/Index/login");
+                }
             }
         }
+
     }
 }
