7fadb9e296d91a490748de230707616f714c8ea0
lijian <lijianwzx@163.com>
2020-08-13 20:18:04 +0800

    1.0

--- application/common/validate/Article.php
+++ application/common/validate/Article.php
@@ -8,7 +8,6 @@ class Article extends Validate
 {
     protected $rule = [
         'title|文章标题'=>'require',
-        'tags|文章标签'=>'require',
         'cateid|所属栏目'=>'require',
         'desc|文章摘要'=>'require|max:100',
         'content|文章内容'=>'require',
@@ -22,12 +21,12 @@ class Article extends Validate
 
     public function sceneAdd()
     {
-        return $this->only(['title','desc','content', 'tags','cateid','member_id']);
+        return $this->only(['title','desc','content','cateid','member_id']);
     }
 
  
     public function sceneEdit()
     {
-        return $this->only(['title','desc', 'tags','content','cateid']);
+        return $this->only(['title','desc','content','cateid']);
     }
 }
